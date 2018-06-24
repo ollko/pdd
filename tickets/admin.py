b37e10f0951dcf8d50153c8bd4733d24106473fe
Olga <korotkaya.olga@yandex.ru>
2018-06-24 19:46:07 +0300

    admin.py

--- tickets/admin.py
+++ tickets/admin.py
@@ -17,6 +17,7 @@ class ChoiceItemInline(admin.TabularInline):
 
 class QuestionAdmin(admin.ModelAdmin):
     list_display_links = ["question"]
+    # search_fields = ["ticket", "theme"]
     list_display = (
                     'id',
                     'question', 
